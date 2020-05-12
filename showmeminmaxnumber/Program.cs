7f081f87550c932bc9f1d78242cef346428323a3
Minji Kim <s2018s24@e-mirim.hs.kr>
2020-05-12 12:43:47 +0900

    5개 수 중에서 min, max 출력

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,30 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] num = new int[5];
+            int max, min;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            max = num[0]; min = num[0];
+            for(int i=1;i<5;i++)
+            {
+                if (num[i] > max)
+                {
+                    max = num[i];
+                }
+                if (num[i] < min)
+                {
+                    min = num[i];
+                }
+            }
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("가장 큰 수: "+ max);
+            Console.WriteLine("가장 작은 수: " + min);
         }
     }
 }
